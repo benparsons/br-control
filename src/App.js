a3b906783079b22c112177bfe4524ffc96311895
Ben Parsons <ben@bpulse.co.uk>
2019-11-16 13:59:11 +0000

    factor out into TaskList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import moment from 'moment';
+import TaskList from './TaskList';
 import TagsList from './TagsList';
 import { TaskRow } from './TaskRow';
 import { ProjectBlock } from './ProjectBlock';
@@ -15,53 +16,7 @@ export function doOpen(name) {
     });
 }
 
-class TaskList extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      tasks: []
-    };
-  }
-  componentDidMount() {
-    var url = `http://localhost:1428/tasks/${this.props.statuses}/${this.props.datemode}/${this.props.count}`;
-    axios.get(url)
-      .then(res => {
-        this.setState({
-          tasks: res.data
-        });
-
-        res.data.forEach(task => {
-          if (!task.fm.tags) return;
-          task.fm.tags.forEach(tag => {
-            this.props.addTag(tag);
-          });
-        });
-      });
-  }
-  render() {
-    const lines = this.state.tasks.map((task, index) => {
-      var due = task.fm.task.due
-        ? moment(task.fm.task.due).format('YYYY-MM-DD')
-        : "";
-
-      return (
-        <TaskRow
-          key={task.name}
-          name={task.name}
-          due={due}
-          title={task.title}
-          status={task.fm.task.status}
-          tags={task.fm.tags}
-          filterTag={this.props.filterTag}
-        />
-      );
-    });
-    return (
-      <table style={{width: "100%"}}><tbody>{lines}</tbody></table>
-    )
-  }
-}
 
 class DailyTaskList extends Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import Tasks from './Tasks';
 import Diary from './Diary';
 import EntryView from './EntryView';
 import TagsList from './TagsList';
@@ -12,6 +13,7 @@ const routing = (
     <Router>
       <div>
         <Route exact path="/" component={App} />
+        <Route exact path="/tasks" component={Tasks} />
         <Route path="/diary" component={Diary} />
         <Route path="/tags" component={TagsList} />
         <Route path="/*.html" component={EntryView} />
