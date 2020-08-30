bb439e573797367c2fef02828352cf540a09a1ce
Ben Parsons <ben@bpulse.co.uk>
2020-08-30 15:00:23 +0100

    button controls

--- src/TaskRow.js
+++ src/TaskRow.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import { doOpen } from './App';
+import axios from 'axios';
 export class TaskRow extends Component {
+  setDone(name) {
+
+  var url = `http://localhost:1428/set-field/${name}/task.status/done`;
+  axios.get(url)
+    .then(res => {
+      console.log(res);
+    });
+    
+    alert(name);
+  }
   render() {
     const taskClass = moment().isAfter(this.props.due) ? "overdue" : "";
     return (<>
@@ -11,7 +22,10 @@ export class TaskRow extends Component {
       <td style={{width:"35%"}} onClick={() => { doOpen(this.props.name); }}>{this.props.title}</td>
       <td style={{width:"10%"}}>{this.props.status}</td>
       <td style={{width:"35%"}}>{JSON.stringify(this.props.tags, null, 1)}</td>
-      <td style={{width:"10%"}}><button onClick={() => { doOpen(this.props.name); }}>Open</button></td>
+      <td style={{width:"10%"}}>
+        <button onClick={() => { doOpen(this.props.name); }}>Open</button>
+        <button onClick={() => { this.setDone(this.props.name); }}>Done</button>
+      </td>
     </tr>:null}</>);
   }
 }
