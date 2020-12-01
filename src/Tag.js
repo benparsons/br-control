0c82e91d740e52e7a7d0d163174a8c39e713f19e
Ben Parsons <ben@bpulse.co.uk>
2020-12-01 00:03:24 +0000

    get related tags and entries

--- src/Tag.js
+++ src/Tag.js
@@ -4,40 +4,64 @@ import axios from 'axios';
 import { ProjectBlock } from './ProjectBlock';
 
 class Tag extends Component {
-    constructor(props) {
-      super(props);
-      
-      this.state = {
-        tags: {}
-      };
-    }
-    componentDidMount() {
-        const regex = /\/tag\/(.*)$/;
-        const str = this.props.location.pathname;
-        const out = str.match(regex);
-        console.log(out);
-        var url = `http://localhost:1428/tag/${out[1]}`;
-        axios.get(url)
-            .then(res => {
-                console.log(res.data);
-                this.setState({
-                    tags: {[out[1]]: res.data}
-                });
-            });
-    }
-  
-    render() {
-  
-      var tags = [];
-      console.log(this.state.tags);
-      Object.keys(this.state.tags).forEach(name =>{
-        tags.push(<ProjectBlock key={name} name={name} tasks={this.state.tags[name]} />);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tags: {}
+    };
+  }
+  componentDidMount() {
+    const regex = /\/tag\/(.*)$/;
+    const str = this.props.location.pathname;
+    const out = str.match(regex);
+    this.getTag(out[1], true)
+  }
+
+  getTag(tag, recurse) {
+    if (this.state.tags[tag]) return;
+
+    var url = `http://localhost:1428/tag/${tag}`;
+    axios.get(url)
+      .then(res => {
+        let stateTags = this.state.tags;
+        stateTags[tag] = res.data;
+        this.setState({
+          tags: stateTags
+        });
+        if (recurse) { this.getRelatedTags(); }
       });
-      
-      return <div className="tasklist-block">{tags}</div>
-      
-      //return <div className="tasklist-block">{JSON.stringify(this.state.tags)}</div>;
-    }
+
+  }
+
+  getRelatedTags() {
+    Object.keys(this.state.tags).forEach(tagName => {
+      this.state.tags[tagName].forEach(entry => {
+        let url = `http://localhost:1428/get/${entry.name}`;
+
+        //console.log(url);
+        axios.get(url)
+          .then(res => {
+            if (!res.data.fm && res.data.fm.tags) return;
+            res.data.fm.tags.forEach(newTag => {
+              this.getTag(newTag, false);
+            })
+          });
+
+      })
+    })
+  }
+
+  render() {
+    var tags = [];
+    Object.keys(this.state.tags).forEach(name => {
+      tags.push(<ProjectBlock key={name} name={name} tasks={this.state.tags[name]} />);
+    });
+
+    return <div className="tasklist-block">{tags}</div>
+
+    //return <div className="tasklist-block">{JSON.stringify(this.state.tags)}</div>;
+  }
 }
 
 export default Tag
